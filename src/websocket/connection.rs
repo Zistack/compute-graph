062a7084b0672c56fccebfd720c8cd18364b02f4
Zistack <Zistack@null.net>
2025-04-12 18:56:57 -0400

    Added a number of websocket node variants

--- src/websocket/connection.rs
+++ src/websocket/connection.rs
@@ -18,9 +18,12 @@ use super::keepalive::*;
 
 use crate as compute_graph;
 
+// My feed! macros don't attempt to flush the stream.  I could manually add
+// flush calls, or I could make an analogous send! macro.
+
 #[expand_streams]
 #[service (shutdown = shutdown)]
-pub async fn websocket_node_wtih_pings <IS, IF, OS, OF, S>
+pub async fn websocket_node_wtih_pings <IF, OF, IS, OS, S>
 (
 	inputs: input! (IS -> impl Into <IF::Intermediate>),
 	outputs: output! (OS <- OF::External),
@@ -80,7 +83,227 @@ where
 	{
 		let (mut websocket_sink, _) = input_report . split ();
 
-		feed! (websocket_sink?, Message::Close (None));
+		feed! (websocket_sink, Message::Close (None));
+
+		WithStatus::from (ExitStatus::Clean)
+	}
+}
+
+#[expand_streams]
+#[service (shutdown = shutdown)]
+pub async fn websocket_node <IF, OF, IS, OS, S>
+(
+	inputs: input! (IS -> impl Into <IF::Intermediate>),
+	outputs: output! (OS <- OF::External),
+	websocket: WebSocketStream <S>
+)
+-> WithStatus
+where
+	IF: InputFormat,
+	OF: OutputFormat,
+	S: AsyncRead + AsyncWrite + Unpin + Debug
+{
+	let (websocket_sink, websocket_stream) = websocket . split ();
+
+	let shuttle_input_handle =
+		shuttle_input::<IF, _, _> (inputs, websocket_sink);
+	let shuttle_output_handle =
+		shuttle_output::<OF, _, _> (websocket_stream, outputs);
+
+	let (input_report, output_report) = join_services!
+	(
+		?shutdown,
+		shuttle_input_handle,
+		shuttle_output_handle
+	);
+
+	if input_report . status_spurious () || output_report . status_spurious ()
+	{
+		WithStatus::from (ExitStatus::Spurious)
+	}
+	else
+	{
+		let (mut websocket_sink, _) = input_report . split ();
+
+		feed! (websocket_sink, Message::Close (None));
+
+		WithStatus::from (ExitStatus::Clean)
+	}
+}
+
+#[expand_streams]
+#[service (shutdown = shutdown)]
+pub async fn websocket_source_with_pings <OF, OS, S>
+(
+	outputs: output! (OS <- OF::External),
+	websocket: WebSocketStream <S>,
+	ping_interval: Duration,
+	ping_timeout: Duration
+)
+-> WithStatus
+where
+	OF: OutputFormat,
+	S: AsyncRead + AsyncWrite + Unpin + Debug
+{
+	let (websocket_sink, websocket_stream) = websocket . split ();
+
+	let (pong_sender, pong_receiver) = mpsc::channel (1);
+
+	let shuttle_output_handle = shuttle_output_with_pongs::<OF, _, _, _>
+	(
+		websocket_stream,
+		outputs,
+		PollSender::new (pong_sender)
+	);
+
+	let keepalive_handle = keepalive_direct_pings
+	(
+		websocket_sink,
+		ReceiverStream::new (pong_receiver),
+		ping_interval,
+		ping_timeout
+	);
+
+	let (output_report, keepalive_report) = join_services!
+	(
+		?shutdown,
+		shuttle_output_handle,
+		keepalive_handle
+	);
+
+	if output_report . status_spurious () || keepalive_report . status_spurious ()
+	{
+		WithStatus::from (ExitStatus::Spurious)
+	}
+	else
+	{
+		let (mut websocket_sink, _) = keepalive_report . split ();
+
+		feed! (websocket_sink, Message::Close (None));
+
+		WithStatus::from (ExitStatus::Clean)
+	}
+}
+
+#[expand_streams]
+#[service (shutdown = shutdown)]
+pub async fn websocket_source <OF, OS, S>
+(
+	outputs: output! (OS <- OF::External),
+	websocket: WebSocketStream <S>
+)
+-> WithStatus
+where
+	OF: OutputFormat,
+	S: AsyncRead + AsyncWrite + Unpin + Debug
+{
+	let (mut websocket_sink, websocket_stream) = websocket . split ();
+
+	let shuttle_output_handle =
+		shuttle_output::<OF, _, _> (websocket_stream, outputs);
+
+	let (output_report,) = join_services! (?shutdown, shuttle_output_handle);
+
+	if output_report . status_spurious ()
+	{
+		WithStatus::from (ExitStatus::Spurious)
+	}
+	else
+	{
+		feed! (websocket_sink, Message::Close (None));
+
+		WithStatus::from (ExitStatus::Clean)
+	}
+}
+
+#[expand_streams]
+#[service (shutdown = shutdown)]
+pub async fn websocket_sink_with_pings <IF, IS, S>
+(
+	inputs: input! (IS -> impl Into <IF::Intermediate>),
+	websocket: WebSocketStream <S>,
+	ping_interval: Duration,
+	ping_timeout: Duration
+)
+-> WithStatus
+where
+	IF: InputFormat,
+	S: AsyncRead + AsyncWrite + Unpin + Debug
+{
+	let (websocket_sink, websocket_stream) = websocket . split ();
+
+	let (ping_sender, ping_receiver) = mpsc::channel (1);
+
+	let shuttle_input_handle = shuttle_input_with_pings::<IF, _, _, _>
+	(
+		inputs,
+		ReceiverStream::new (ping_receiver),
+		websocket_sink
+	);
+
+	let keepalive_handle = keepalive_direct_pongs
+	(
+		PollSender::new (ping_sender),
+		websocket_stream,
+		ping_interval,
+		ping_timeout
+	);
+
+	let (input_report, keepalive_report) = join_services!
+	(
+		?shutdown,
+		shuttle_input_handle,
+		keepalive_handle
+	);
+
+	if input_report . status_spurious () || keepalive_report . status_spurious ()
+	{
+		WithStatus::from (ExitStatus::Spurious)
+	}
+	else
+	{
+		let (mut websocket_sink, _) = input_report . split ();
+
+		feed! (websocket_sink, Message::Close (None));
+
+		WithStatus::from (ExitStatus::Clean)
+	}
+}
+
+#[expand_streams]
+#[service (shutdown = shutdown)]
+pub async fn websocket_sink <IF, IS, S>
+(
+	inputs: input! (IS -> impl Into <IF::Intermediate>),
+	websocket: WebSocketStream <S>
+)
+-> WithStatus
+where
+	IF: InputFormat,
+	S: AsyncRead + AsyncWrite + Unpin + Debug
+{
+	let (websocket_sink, websocket_stream) = websocket . split ();
+
+	let shuttle_input_handle =
+		shuttle_input::<IF, _, _> (inputs, websocket_sink);
+	let drain_handle = drain_output (websocket_stream);
+
+	let (input_report, output_report) = join_services!
+	(
+		?shutdown,
+		shuttle_input_handle,
+		drain_handle
+	);
+
+	if input_report . status_spurious () || output_report . status_spurious ()
+	{
+		WithStatus::from (ExitStatus::Spurious)
+	}
+	else
+	{
+		let (mut websocket_sink, _) = input_report . split ();
+
+		feed! (websocket_sink, Message::Close (None));
 
 		WithStatus::from (ExitStatus::Clean)
 	}
